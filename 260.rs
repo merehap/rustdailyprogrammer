73f144eaad4aaa81630552f6737fbe5e6d5af43f
sean <merehap@gmail.com>
2016-04-08 21:54:50 -0700

    Challenge 260 bonus.

--- 260.rs
+++ 260.rs
@@ -3,11 +3,23 @@ use Direction::*;
 use Event::*;
 
 fn main() {
-    let events = vec![Click, CycleCompleted, Click, Click, Click, Click, Click, CycleCompleted];
+    let events = vec![
+        Click,
+        CycleCompleted,
+        Click,
+        BlockDetected,
+        Click,
+        CycleCompleted,
+        Click,
+        BlockCleared,
+        Click,
+        CycleCompleted,
+    ];
 
     let mut state = State {
         completion_state: Complete,
         direction: Closing,
+        blocked: false,
     };
 
     for event in events {
@@ -19,22 +31,27 @@ fn main() {
     println!("Door: {:?} {:?}", state.completion_state, state.direction);
 }
 
+#[derive(Debug)]
 struct State {
     completion_state: CompletionState,
     direction: Direction,
+    blocked: bool,
 }
 
 impl State {
-    fn new(completion_state: CompletionState, direction: Direction) -> State {
-        State { completion_state: completion_state, direction: direction }
+    fn new(completion_state: CompletionState, direction: Direction, blocked: bool) -> State {
+        State { completion_state: completion_state, direction: direction, blocked: blocked }
     }
 
     fn transition(&self, event: Event) -> State {
-        match (self.completion_state.clone(), self.direction.clone(), event) {
-            (InProgress, direction, Click         ) => State::new(Stopped   , direction),
-            (InProgress, direction, CycleCompleted) => State::new(Complete  , direction),
-            (_         , direction, Click         ) => State::new(InProgress, direction.flip()),
-            _ => panic!("Invalid input"),
+        match (self.completion_state.clone(), self.direction.clone(), self.blocked, event.clone()) {
+            (completion, direction, _      , BlockCleared  ) => State::new(completion, direction       , false  ),
+            (InProgress, Closing  , _      , BlockDetected ) => State::new(InProgress, Opening         , true   ),
+            (InProgress, direction, blocked, CycleCompleted) => State::new(Complete  , direction       , blocked),
+            (completion, direction, true   , _             ) => State::new(completion, direction       , true   ),
+            (InProgress, direction, blocked, Click         ) => State::new(Stopped   , direction       , blocked),
+            (_         , direction, blocked, Click         ) => State::new(InProgress, direction.flip(), blocked),
+            _ => panic!(format!("Invalid input. Event: {:?} . State: {:?} ", event, self)),
         }
     }
 }
@@ -61,8 +78,10 @@ impl Direction {
     }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 enum Event {
     Click,
     CycleCompleted,
+    BlockDetected,
+    BlockCleared,
 }
