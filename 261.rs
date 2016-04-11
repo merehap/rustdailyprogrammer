1b3f458f10c493f8733e648321494f43c03d0c54
sean <merehap@gmail.com>
2016-04-10 19:17:22 -0700

    Updated 261 to work with any size of magic square.

--- 261.rs
+++ 261.rs
@@ -1,57 +1,67 @@
 
 fn main() {
     let squares = vec![
-        [[8, 1, 6],
-         [3, 5, 7],
-         [4, 9, 2]],
+        vec![vec![8, 1, 6],
+             vec![3, 5, 7],
+             vec![4, 9, 2]],
 
-         [[2, 7, 6],
-          [9, 5, 1],
-          [4, 3, 8]],
+         vec![vec![2, 7, 6],
+              vec![9, 5, 1],
+              vec![4, 3, 8]],
 
-         [[3, 5, 7],
-          [8, 1, 6],
-          [4, 9, 2]],
+         vec![vec![3, 5, 7],
+              vec![8, 1, 6],
+              vec![4, 9, 2]],
 
-         [[8, 1, 6],
-          [7, 5, 3],
-          [4, 9, 2]],
+         vec![vec![8, 1, 6],
+              vec![7, 5, 3],
+              vec![4, 9, 2]],
     ];
 
     for square in squares {
-        println!("{:?}: {:?}", square, verify_magic_square(&square));
+        println!("{:?}: {:?}", square, verify_magic_square(15, &square));
     }
 }
 
-fn verify_magic_square(square: &[[u8; 3]; 3]) -> bool {
-    for row in 0..3 {
+fn verify_magic_square(magic_number: u8, square: &Vec<Vec<u8>>) -> bool {
+    let len = square.len();
+    for row in 0..len {
         let mut sum = 0;
-        for col in 0..3 {
-            sum += square[row][col];        
+        for col in 0..len {
+            sum += square[row][col]; 
         }
 
-        if sum != 15 {
+        if sum != magic_number {
             return false;
         }
     }
 
-
-    for col in 0..3 {
+    for col in 0..len {
         let mut sum = 0;
-        for row in 0..3 {
+        for row in 0..len {
             sum += square[row][col];        
         }
 
-        if sum != 15 {
+        if sum != magic_number {
             return false;
         }
     }
 
-    if square[0][0] + square[1][1] + square[2][2] != 15 {
+    let mut sum = 0;
+    for index in 0..len {
+        sum += square[index][index];    
+    }
+
+    if sum != magic_number {
         return false;
     }
 
-    if square[2][0] + square[1][1] + square[0][2] != 15 {
+    sum = 0;
+    for index in 0..len {
+        sum += square[len - index - 1][index];    
+    }
+
+    if sum != magic_number {
         return false;
     }
 
