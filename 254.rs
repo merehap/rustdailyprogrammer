7d8869e914c3fd910e14b8893ab6b35af7f08311
sean <merehap@gmail.com>
2016-04-14 19:29:49 -0700

    254 bonus.

--- 254.rs
+++ 254.rs
@@ -1,7 +1,7 @@
 
 fn main() {
 
-    let inputs = vec!["foobar", "wizard", "/r/dailyprogrammer", "gsrh rh zm vcznkov lu gsv zgyzhs xrksvi"];
+    let inputs = vec!["Foobar", "WIZARD", "/r/dailYprogrammer", "Gsrh rh Zm vcznKov lu gSv zgyzHS xrksvi"];
     for input in inputs {
         println!("{}", atbash(input.to_owned()));
     }
@@ -12,6 +12,7 @@ fn atbash(text: String) -> String {
     for letter in text.chars() {
         let rev = match letter {
             'a'...'z' => (25 - ((letter as u8) - ('a' as u8)) + ('a' as u8)) as char,
+            'A'...'Z' => (25 - ((letter as u8) - ('A' as u8)) + ('A' as u8)) as char,
             _ => letter,
         };
 
